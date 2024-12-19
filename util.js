b48fb52b439b68ea3b1b0721f259ef3a98f34b51
Rafael Gomes <rafaelmgomes.dev@gmail.com>
2024-12-18 21:51:57 -0300

    Commit 9 - Criando Arquivos

---
+++ util.js
@@ -0,0 +1,9 @@
+// arquivo util.js
+
+module.exports = function formatarMoeda(valor) {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  }).format(valor / 100);
+};
